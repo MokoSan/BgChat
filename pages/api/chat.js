e408a3089231cb69da39d5a27dc85c9a9e60ef81
Mukund Raghav Sharma (Moko) <mukund.sharma92@gmail.com>
2023-05-18 16:30:55 -0700

    More details

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,15 +1,27 @@
+const { Configuration, OpenAIApi } = require("openai");
+
 export default async function (req, res) {
 
-  const response = await fetch("http://mrsharm.pythonanywhere.com/bg", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      Query: req.body.question,
-    }),
-  });
+  const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY, });
+  const openai = new OpenAIApi(configuration);
 
-    const data = await response.json();
+  const prompt = `You a very helpful curator of the Bhagwat Gita and answer questions with considerable depth and clarity. 
+  The answers should all be complete.
+  Answer the question in the <tag></tag>: <tag>${req.body.question}</tag>
+  `
+
+  const response = await openai.createCompletion({
+    //model: "gpt-3.5-turbo-0301",
+    model: "text-davinci-003",
+    prompt: prompt, //req.body.question, 
+    temperature: 0,
+    max_tokens: 400,
+    top_p: 1.0,
+    frequency_penalty: 0.5,
+    presence_penalty: 0.0//,
+    });
+
+    const data = response.data.choices[0].text;
+    console.log(data)
     res.status(200).json({ result: data })
 }
